fae634da027d698e96e4a72d272e64d8267d10dc
jianan3 <jianan3@vipkid.com.cn>
2019-08-16 14:08:46 +0800

    life

--- ddz-client/Scripts/start.js
+++ ddz-client/Scripts/start.js
@@ -5,15 +5,18 @@ qc.landlord = {};
 window.G = qc.landlord.G = {};
 
 // 初始化逻辑
-qc.initGame = function(game) {
+qc.initGame = function (game) {
     game.log.trace('Start the game landlord.');
+
     // 将game实例的引用记录下来，方便在其他逻辑脚本模块中访问
     G.game = game;
+
     //游戏规则
     G.gameRule = new qc.landlord.GameRule();
 
     G.online = new qc.landlord.Online();
-    //AI逻辑
-    //G.AILogic = new qc.landlord.AILogic();
-    String.prototype.trim = function() {  return this.replace(/(^\s*)|(\s*$)/g,'');  };
+
+    String.prototype.trim = function () {
+        return this.replace(/(^\s*)|(\s*$)/g, '');
+    };
 };
